8fb35af358ce7d8dfd38f45563ae7a3751bd8396
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2022-08-03 18:51:32 +0200

    remove unused .js files

--- packages/typo3_vite_demo/Resources/Public/JavaScript/Dist/scripts.js
+++
@@ -1,6 +0,0 @@
-/*!
- * typo3-vite-demo v1.0.0 (https://florian.geierstanger.org/)
- * Copyright 2017-2022 Florian Geierstanger
- * Licensed under the GPL-2.0-or-later license
- */
-console.log("WE LOVE TYPO3");
