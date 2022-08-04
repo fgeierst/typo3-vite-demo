b44592cea7cb3e2ed925cf74756be44fe241dd98
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2022-08-04 17:47:13 +0200

    add ddev-viteserve add-on

---
+++ .ddev/viteserve/build-dotenv.sh
@@ -0,0 +1,73 @@
+#! /usr/bin/env bash
+
+y_flag=''
+
+print_usage() {
+  printf "Usage: $0 [-y]"
+}
+
+while getopts 'y' flag; do
+  case "${flag}" in
+  y) y_flag='true' ;;
+  *)
+    print_usage
+    exit 1
+    ;;
+  esac
+done
+
+# @see https://stackoverflow.com/a/27650122/8600734
+mapfile VITE_SETTINGS <<'VITE'
+# start vite
+VITE_PROJECT_DIR=frontend
+VITE_PRIMARY_PORT=5173
+VITE_SECONDARY_PORT=5273
+# end vite
+VITE
+
+echo "New settings are:"
+printf %s "${VITE_SETTINGS[@]}"
+echo "--------------"
+echo
+
+cd .ddev
+
+if [ -f "./.env" ]; then
+  if grep "^# start vite" .env >/dev/null; then
+    echo "Found vite settings in your .ddev/.env file."
+    if [ -z "$y_flag" ]; then
+      echo -n "Replace old settings (y/n)? "
+      read replace
+    else
+      replace=y
+    fi
+    if [ "$replace" == "y" ]; then
+      sed -i.bak '/^# start vite/,/^\# end vite/d;' .env
+      printf %s "${VITE_SETTINGS[@]}" >>.env
+    else
+      echo "Skipping changes to your .env file."
+    fi
+  else
+    echo "You have an existing .ddev/.env file."
+    if [ -z "$y_flag" ]; then
+      echo "May I append settings for this add-on to"
+      echo -n "your .env file (y/n)? "
+      read replace
+    else
+      replace=y
+    fi
+
+    if [ "$replace" == "y" ]; then
+      printf %s "${VITE_SETTINGS[@]}" >>.env
+    else
+      echo "Skipping changes to your .env file."
+    fi
+  fi
+else
+  echo "Create a .ddev/.env file with your add-on settings:"
+  printf %s "${VITE_SETTINGS[@]}" >.env
+fi
+echo
+echo "Your .ddev/.env file now contains:"
+echo "----------------------------------"
+cat .env
---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,116 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'mysqli',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'FE' => [
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pagesection' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '31f4d5949e6677049d31b681c50f5f23cc392e56327b8596370f9ab315fb287b56ee6500d154c9b0d5f54035104d43c3',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'unifiedPageTranslationHandling' => true,
+            'yamlImportsFollowDeclarationOrder' => true,
+        ],
+        'sitename' => 'New TYPO3 site',
+    ],
+];
