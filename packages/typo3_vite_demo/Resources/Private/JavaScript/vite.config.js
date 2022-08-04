6803fc5e96bd3cdb175eaf69b9bccead8ec3823c
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2022-08-04 17:49:50 +0200

    Move JS to base package

---
+++ packages/typo3_vite_demo/Resources/Private/JavaScript/vite.config.js
@@ -0,0 +1,12 @@
+export default {
+  publicDir: false, // disable copy `public/` to outDir
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: 'packages/typo3_vite_demo/Resources/Private/JavaScript/main.js'
+    },
+    outDir: 'packages/typo3_vite_demo/Resources/Public/JavaScript/',
+  }
+}
