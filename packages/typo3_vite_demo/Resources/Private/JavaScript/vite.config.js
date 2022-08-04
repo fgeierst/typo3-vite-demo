b1e85a81f8b92fc0a5d994dfd083202a40d8bf4c
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2022-08-04 19:17:46 +0200

    Correct paths

--- packages/typo3_vite_demo/Resources/Private/JavaScript/vite.config.js
+++ packages/typo3_vite_demo/Resources/Private/JavaScript/vite.config.js
@@ -1,11 +1,14 @@
 export default {
+  server: {
+    origin: 'https://typo3-vite-demo.ddev.site/'
+  },
   publicDir: false, // disable copy `public/` to outDir
   build: {
     // generate manifest.json in outDir
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: 'packages/typo3_vite_demo/Resources/Private/JavaScript/src/main.js'
+      input: 'packages/typo3_vite_demo/Resources/Private/JavaScript/main.js'
     },
     outDir: 'packages/typo3_vite_demo/Resources/Public/JavaScript/',
   }
