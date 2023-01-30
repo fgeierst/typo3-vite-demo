667b281d28a081b03de19c4c48a568a1dc82bba5
Peter Neumann <mail@peter-neumann.dev>
2023-01-30 21:19:08 +0100

    Drop need of custom UserFunction and remove hashes by Vite

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,15 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: 'packages/typo3_vite_demo/Resources/Private/JavaScript/main.js', // overwrite default .html entry
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
     },
-    manifest: true, // generate manifest.json in outDir
-    outDir: 'packages/typo3_vite_demo/Resources/Public/Vite/', // ⚠️ `pnpm build` purges outDir
+    outDir: 'packages/typo3_vite_demo/Resources/Public/',
+    assetsDir: '',
+    emptyOutDir: false,
   },
   css: {
     devSourcemap: true, // disabled by default because of performance reasons
